42b6d49e734c96381d9cba1a7accf39c1c0d58ee
djtoler <84260551+djtoler@users.noreply.github.com>
2022-07-11 16:32:45 -0400

    email changed

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'oboyz.investing@gmail.com',
+    email: 'oceanboyzinvestments@gmail.com',
     phone: '313-555-5555',
     address: 'Detroit, Michigan, United States ',
 
